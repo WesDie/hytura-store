89c212356ebcbe2cd209cd24998a0275ff0e9cb5
WesDie <wes.dieleman@gmail.com>
2024-06-28 16:07:59 +0200

    chore: changed next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
