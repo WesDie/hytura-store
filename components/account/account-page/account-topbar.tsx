4fb0c40af5acd906a63ac8e306553cc4d32179d9
WesDie <wes.dieleman@gmail.com>
2024-07-04 21:44:59 +0200

    feat: add ability see diffrence between tabs in account pages

--- components/account/account-page/account-topbar.tsx
+++ components/account/account-page/account-topbar.tsx
@@ -1,17 +1,24 @@
 "use client";
 import { shopifyLogoutCustomer } from "../actions";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Button from "../../elements/button";
+import { useState, useEffect } from "react";
 
 export default function AccountTopBar({ firstName }: { firstName: string }) {
   const router = useRouter();
+  const pathname = usePathname();
+  const [activeTab, setActiveTab] = useState("/account/general-information");
 
   const logout = async () => {
     await shopifyLogoutCustomer();
     router.push("/");
     router.refresh();
   };
+
+  useEffect(() => {
+    setActiveTab(pathname);
+  }, [pathname]);
 
   return (
     <div className="flex flex-col gap-2x pt-[96px]">
@@ -20,12 +27,15 @@ export default function AccountTopBar({ firstName }: { firstName: string }) {
       </h1>
       <div className="flex justify-between border-y border-stroke-gray px-4x py-2x">
         <div className="flex gap-2x">
-          <Link href={"/account"} className="text-link-sm text-text-black">
+          <Link
+            href={"/account/general-information"}
+            className={`text-link-sm ${activeTab === "/account/general-information" ? "text-text-black" : "text-text-light-gray"}`}
+          >
             General info
           </Link>
           <Link
             href={"/account/orders"}
-            className="text-link-sm text-text-light-gray"
+            className={`text-link-sm ${activeTab === "/account/orders" ? "text-text-black" : "text-text-light-gray"}`}
           >
             Orders
           </Link>
