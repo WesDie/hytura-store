bdfa3502d1dfa32441ead5e00bdb3916b9740e25
WesDie <wes.dieleman@gmail.com>
2024-07-15 19:56:21 +0200

    feat: add product usp component

---
+++ components/product/product-usps.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function ProductUsps() {
+  return (
+    <ul className="flex flex-col gap-[4px]">
+      <li className="text-body-xs ml-2x list-disc text-text-light-gray">
+        Delivered the next day
+      </li>
+      <li className="text-body-xs ml-2x list-disc text-text-light-gray">
+        Happy plants
+      </li>
+    </ul>
+  );
+}
