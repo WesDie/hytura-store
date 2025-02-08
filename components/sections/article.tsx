b6957fe886cdc2fe75c39e72c06fc9d1b568a799
WesDie <wes.dieleman@gmail.com>
2025-02-08 14:06:52 +0100

    style: improve texts spacing and size of article

--- components/sections/article.tsx
+++ components/sections/article.tsx
@@ -11,25 +11,25 @@ export default function ArticleDetails({ article }: { article: Article }) {
         <div className="flex w-full px-2x pb-4x pt-5x md:sticky md:-top-[200px] md:h-[500px] md:p-4x">
           <div className="mt-auto flex flex-col gap-1x">
             <div className="flex gap-2x">
-              <div className="flex h-5x w-5x rounded-full bg-background-dark-sand">
+              <div className="flex h-6x w-6x rounded-full bg-background-dark-sand">
                 <p className="text-heading-4xs m-auto text-text-black">
                   {authorInitials}
                 </p>
               </div>
               <div className="my-auto flex flex-col">
-                <p className="text-heading-4xs text-text-light-gray">
+                <p className="text-heading-3xs text-text-light-gray">
                   {article.publishedAt.split("T")[0]}
                 </p>
-                <p className="text-heading-4xs text-text-light-gray">
+                <p className="text-heading-3xs text-text-light-gray">
                   By {article.author.name}
                 </p>
               </div>
             </div>
-            <div className="flex w-full flex-col gap-1x md:gap-2x">
+            <div className="flex w-full flex-col gap-1x md:gap-1x">
               <h1 className="text-heading-md text-heading-sm">
                 {article.title}
               </h1>
-              <p className="text-body-sm text-body-xs">{article.excerpt}</p>
+              <p className="text-body-sm">{article.excerpt}</p>
             </div>
           </div>
         </div>
